dab5456a1f19e7d71a0a2589d2af83460fa0abdc
THWE THWE AUNG <w25013@osfl.ac.jp>
2026-06-09 14:45:28 +0900

    Basic認証を適用

---
+++ app/_components/Header/index.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import Link from "next/link";
+import Styles from "./index.module.css";
+import Menu from "../Menu";
+
+
+export default function Header(){
+    return (
+        <header className = {Styles.header}>
+            < Link href = "/" className = { Styles.logoLink}>
+            <Image 
+            src="/logo.svg"
+            alt="Logo"
+            className={Styles.logo}
+            width={348}
+            height={133}
+            priority
+            />
+            </Link>
+            <Menu />
+            
+            </header>
+    );
+}
---
+++ app/members/layout.tsx
@@ -0,0 +1,20 @@
+
+import Sheet from "@/app/_components/Sheet";
+import Hero from "@/app/_components/Hero";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export default function MembersLayout({ children }: Props) {
+    // 
+    return (
+        <>
+        <Hero title="Members" sub="メンバー" />
+        
+        <Sheet>
+            {children}
+        </Sheet>
+        </>
+    );
+}
