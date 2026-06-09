bd76405b2350a1d33af8b743f6ed13585f5a305d
THWE THWE AUNG <w25013@osfl.ac.jp>
2026-06-09 15:10:36 +0900

    Fix Header JSX spacing/attributes

--- app/_components/Header/index.tsx
+++ app/_components/Header/index.tsx
@@ -7,15 +7,15 @@ import Menu from "../Menu";
 export default function Header(){
     return (
         <header className = {Styles.header}>
-            < Link href = "/" className = { Styles.logoLink}>
-            <Image 
-            src="/logo.svg"
-            alt="Logo"
-            className={Styles.logo}
-            width={348}
-            height={133}
-            priority
-            />
+            <Link href="/" className={Styles.logoLink}>
+                <Image
+                    src="/logo.svg"
+                    alt="Logo"
+                    className={Styles.logo}
+                    width={348}
+                    height={133}
+                    priority
+                />
             </Link>
             <Menu />
             
